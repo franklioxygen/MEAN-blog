15182d5c3e0b8aeca2f0a3e8a296cb27e22f8e0c
franklioxygen <frank.li.oxygen@gmail.com>
2016-01-08 00:36:45 +0000

    first built

--- app.js
+++ app.js
@@ -9,10 +9,23 @@ var MongoStore = require('connect-mongo')(session);
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var settings = require('./settings');
-
+var flash = require('connect-flash');
 
 
 var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(flash());
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret: settings.cookieSecret,
@@ -22,22 +35,14 @@ app.use(session({
     db: settings.db,
     host: settings.host,
     port: settings.port,
-    url: 'mongodb://localhost' // *updated
+    url: 'mongodb://localhost/'+settings.db // *updated
   })
 }));
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next){
+    res.locals.session = req.session;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
