774b19d786b4152ab33c1508ce04fd503964aa3c
franklioxygen <frank.li.oxygen@gmail.com>
2016-01-21 22:59:50 +0000

    "$0"

---
+++ public/javascripts/ng-app.js
@@ -0,0 +1,15 @@
+
+var app = angular.module('myapp', ['UserValidation']);
+
+angular.module('UserValidation', []).directive('validPasswordC', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function (viewValue, $scope) {
+                var noMatch = (viewValue !== scope.myForm.password.$viewValue);
+                ctrl.$setValidity('noMatch', !noMatch);
+            })
+        }
+    }
+})
+
